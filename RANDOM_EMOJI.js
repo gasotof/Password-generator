8368c809db77e8d676832442a75f6b6061d38ad7
sorin oprean petru <sorinoprean@hotmail.es>
2025-10-23 13:29:57 +0200

    Cambios de Modulos

---
+++ RANDOM_EMOJI.js
@@ -0,0 +1,9 @@
+import EMOJI_LIST from "./EMOJI_LIST.js";
+
+
+const getRandomEmoji = () => {
+  const index = Math.floor(Math.random() * EMOJI_LIST.length);
+  return EMOJI_LIST[index];
+};
+
+export default getRandomEmoji;
